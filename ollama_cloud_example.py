af6bdc9971b2b191273bfb21af709274660440bb
Rafael Diaz <rafael.diaz.laux@gmail.com>
2025-11-23 16:26:11 +0100

    added local ollama client for embeddings

--- ollama_cloud_example.py
+++ ollama_cloud_example.py
@@ -1,13 +1,27 @@
-import os
+import os, subprocess, httpx
 from dotenv import load_dotenv
 from ollama import Client
+
+def is_ollama_running(host="http://localhost:11434"):
+    try:
+        r = httpx.get(f"{host}/api/tags", timeout=2)
+        return r.status_code == 200
+    except httpx.RequestError:
+        return False
+
+if not is_ollama_running():
+    subprocess.Popen(["ollama", "serve"])
 
 load_dotenv()
 api_key = os.getenv("OLLAMA_API_KEY")
 
-client = Client(
+llm_client = Client(
     host="https://ollama.com",
     headers={"Authorization": f"Bearer {api_key}"}
+)
+
+embed_client = Client(
+    host="http://localhost:11434",
 )
 
 messages = [
@@ -17,5 +31,8 @@ messages = [
     },
 ]
 
-for part in client.chat("gpt-oss:20b-cloud", messages=messages, stream=True):
-    print(part["message"]["content"], end="", flush=True)
+answer = llm_client.chat("gpt-oss:20b-cloud", messages=messages)
+print(answer["message"]["content"])
+
+embed_input = embed_client.embed(model="nomic-embed-text", input=["Hello world", "How are you?"])
+print(embed_input["embeddings"][0][0])
